3353069fff73432da757b39ea91e75b38f907869
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 08:26:31 +0700

    chore: Add Services.php files

---
+++ app/Models/Services.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
+class Services extends Model
+{
+    /** @use HasFactory<\Database\Factories\ServicesFactory> */
+    use HasFactory;
+    protected $table = 'services';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'namespace_id',
+    ];
+    public function namespace() :BelongsTo {
+        return $this->belongsTo(Namespaces::class, 'namespace_id');
+    }
+    public function environments() :BelongsToMany
+    {
+        return $this->belongsToMany(Environments::class, 'services_environments', 'service_id', 'environment_id');
+    }
+    public function servicesEnvironments() : HasMany {
+        return $this->hasMany(ServicesEnvironments::class, 'service_id');
+    }
+    public function configurations() : HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Configurations::class,
+            ServicesEnvironments::class,
+            'service_id',
+            'service_environment_id',
+            'id',
+            'id'
+        );
+    }
+}
