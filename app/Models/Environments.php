b6c10dfde821e08ca4a7de5bf2583c0c25509f05
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 08:26:27 +0700

    chore: Add Environments.php files

---
+++ app/Models/Environments.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
+class Environments extends Model
+{
+    /** @use HasFactory<\Database\Factories\EnvironmentsFactory> */
+    use HasFactory;
+    protected $table = 'environments';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+    ];
+    public function services() :BelongsToMany
+    {
+        return $this->belongsToMany(Services::class, 'services_environments', 'environment_id', 'service_id');
+    }
+    public function servicesEnvironments() : HasMany
+    {
+        return $this->hasMany(ServicesEnvironments::class, 'environment_id');
+    }
+
+    public function configurations() : HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Configurations::class,
+            ServicesEnvironments::class,
+            'environment_id',
+            'service_environment_id',
+            'id',
+            'id'
+        );
+    }
+}
