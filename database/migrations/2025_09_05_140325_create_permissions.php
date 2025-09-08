8ab840585e31747220e59df10aae614f6714cac2
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 08:27:09 +0700

    chore: Add 2025_09_05_140325_create_permissions.php files

---
+++ database/migrations/2025_09_05_140325_create_permissions.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('resource_type')->nullable(false);
+            $table->string('action')->nullable(false);
+            $table->string('description')->nullable(true);
+            $table->unique(['resource_type', 'action']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+};
