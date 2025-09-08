1de6754848ee4861364e47caceea7500d731c75f
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 08:26:28 +0700

    chore: Add Namespaces.php files

---
+++ app/Models/Namespaces.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
+class Namespaces extends Model
+{
+    /** @use HasFactory<\Database\Factories\NamespacesFactory> */
+    use HasFactory;
+    protected $table = 'namespaces';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function services() : HasMany
+    {
+        return $this->hasMany(Services::class, 'namespace_id');
+    }
+
+    public function serviceEnvironments() : HasManyThrough
+    {
+        return $this->hasManyThrough(
+            ServicesEnvironments::class,
+            Services::class,
+            'namespace_id',
+            'service_id',
+            'id',
+            'id'
+        );
+    }
+}
