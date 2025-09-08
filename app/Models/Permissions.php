f1c112e1ac828127d29985b3d19f7fed8f683169
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 08:26:29 +0700

    chore: Add Permissions.php files

---
+++ app/Models/Permissions.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Permissions extends Model
+{
+    /** @use HasFactory<\Database\Factories\PermissionsFactory> */
+    use HasFactory;
+
+    protected $table = 'permissions';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'resource_type',
+        'action',
+        'description',
+    ];
+
+    public function roles() : BelongsToMany
+    {
+        return $this->belongsToMany(Roles::class, 'roles_permissions', 'permission_id', 'role_id');
+    }
+
+    public function rolesPermissions() : HasMany
+    {
+        return $this->hasMany(RolesPermissions::class, 'permission_id');
+    }
+}
