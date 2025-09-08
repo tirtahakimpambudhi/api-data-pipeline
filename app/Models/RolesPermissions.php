d5201f92364333a73bc1360f85b45798e868408d
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 08:26:30 +0700

    chore: Add RolesPermissions.php files

---
+++ app/Models/RolesPermissions.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Couchbase\Role;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class RolesPermissions extends Model
+{
+    /** @use HasFactory<\Database\Factories\RolesPermissionsFactory> */
+    use HasFactory;
+    protected $table = 'roles_permissions';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'role_id',
+        'permission_id',
+    ];
+    public function role() : BelongsTo {
+        return $this->belongsTo(Roles::class, 'role_id', 'id');
+    }
+
+    public function permission() : BelongsTo {
+        return $this->belongsTo(Permissions::class, 'permission_id', 'id');
+    }
+}
