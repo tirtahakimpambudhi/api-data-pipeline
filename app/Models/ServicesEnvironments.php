e03261ce60aefe10c63bf984ed9fd4c243c324b9
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 08:26:32 +0700

    chore: Add ServicesEnvironments.php files

---
+++ app/Models/ServicesEnvironments.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ServicesEnvironments extends Model
+{
+    /** @use HasFactory<\Database\Factories\ServicesEnvironmentsFactory> */
+    use HasFactory;
+    protected $table = 'services_environments';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'service_id',
+        'environment_id',
+    ];
+
+    public function service() : BelongsTo {
+        return $this->belongsTo(Services::class, 'service_id');
+    }
+    public function environment() : BelongsTo {
+        return $this->belongsTo(Environments::class, 'environment_id');
+    }
+    public function channels() : BelongsToMany
+    {
+        return $this->belongsToMany(ServicesEnvironments::class, 'configurations', 'service_environment_id', 'channel_id');
+    }
+    public function configurations() : HasMany
+    {
+        return $this->hasMany(Configurations::class, 'service_environment_id');
+    }
+}
