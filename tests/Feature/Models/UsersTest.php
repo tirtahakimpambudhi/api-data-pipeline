9b6c304a3d3ff7ebbcc23b1135e0ed81802aa88b
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-09 11:31:16 +0700

    chore: Update UsersTest.php files

--- tests/Feature/Models/UsersTest.php
+++ tests/Feature/Models/UsersTest.php
@@ -7,10 +7,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
+
 it('can simulate rbac from users', function () {
     $this->seed(DatabaseSeeder::class);
-
     $users = Users::query()->with(['role', 'role.permissions'])->get();
     $admin = $users->where('role.name', RolesTypes::ALMIGHTY)->first();
-    dd($admin->hasPermission('configurations', 'create'));
+    $this->assertTrue($admin->hasPermission('configurations', 'create'));
 });
