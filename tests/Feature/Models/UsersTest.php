b9810b36e352dd857c8e3e2a91a67deb5f605eda
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-09 09:22:58 +0700

    chore: Add UsersTest.php files

---
+++ tests/Feature/Models/UsersTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Constants\RolesTypes;
+use App\Models\Users;
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('can simulate rbac from users', function () {
+    $this->seed(DatabaseSeeder::class);
+
+    $users = Users::query()->with(['role', 'role.permissions'])->get();
+    $admin = $users->where('role.name', RolesTypes::ALMIGHTY)->first();
+    dd($admin->hasPermission('configurations', 'create'));
+});
