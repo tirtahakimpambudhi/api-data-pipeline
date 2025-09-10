aa083ce05120644606e0cee2ae519fb962282bd6
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-10 09:04:01 +0700

    chore: Add AppServiceException.php files

---
+++ app/Exceptions/AppServiceException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Support\MessageBag;
+use Throwable;
+
+abstract class AppServiceException extends \Exception
+{
+    public function __construct(
+        string $message = 'Something went wrong on service layer.',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function toMessageBag(): MessageBag
+    {
+        return new MessageBag(['errors' => [$this->getMessage()]]);
+    }
+}
