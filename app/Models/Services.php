836c71aada196d9168bb4b2c910b9c0e5e39f067
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-10 15:39:21 +0700

    chore: Update Services.php files

--- app/Models/Services.php
+++ app/Models/Services.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -19,6 +20,22 @@ class Services extends Model
         'name',
         'namespace_id',
     ];
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::get(function ():string {
+            if (!$this->id || !$this->namespace_id) {
+                return '';
+            }
+
+            $this->loadMissing([
+                'namespace',
+            ]);
+
+            return "{$this->namespace->name}.{$this->name}";
+        });
+    }
+
     public function namespace() :BelongsTo {
         return $this->belongsTo(Namespaces::class, 'namespace_id');
     }
