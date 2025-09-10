515dfb68947955e67140d9ce00b4ddc56ae2eacb
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-10 15:39:18 +0700

    chore: Update UpdateNamespaceRequest.php files

--- app/Http/Requests/Namespaces/UpdateNamespaceRequest.php
+++ app/Http/Requests/Namespaces/UpdateNamespaceRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Namespaces;
 
+use App\Exceptions\ValidationServiceException;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateNamespaceRequest extends FormRequest
@@ -22,7 +24,23 @@ class UpdateNamespaceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'string|max:255'
+            'name' => 'string|max:255|unique:namespaces'
         ];
+    }
+
+    public function messages(): array {
+        return [
+            'name.string' => 'Name must be a text!',
+            'name.max' => 'Name cannot be longer than 255 characters!',
+            'name.unique' => 'Name already exists!',
+        ];
+    }
+
+    /**
+     * @throws ValidationServiceException
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new ValidationServiceException(['errors' => $validator->errors()->toArray()]);
     }
 }
