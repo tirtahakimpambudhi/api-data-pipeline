6190091bf9391e7f5348bfe8324f2f189ba59391
thoriqwildan <wildanthoriq14@gmail.com>
2025-09-10 15:51:04 +0700

    Add namespace controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NamespaceController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +12,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::resource('namespaces', NamespaceController::class);
+
+    Route::post('namespaces/{namespace}/services', [NamespaceController::class, 'storeService'])
+        ->name('namespaces.services.store');
 });
 
 require __DIR__.'/settings.php';
