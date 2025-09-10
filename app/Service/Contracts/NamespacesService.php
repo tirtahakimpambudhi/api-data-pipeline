064048ec6f3c996df45631647f1daeecbe0b6d14
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-10 15:39:23 +0700

    chore: Update NamespacesService.php files

--- app/Service/Contracts/NamespacesService.php
+++ app/Service/Contracts/NamespacesService.php
@@ -4,6 +4,10 @@ namespace App\Service\Contracts;
 
 use App\Http\Requests\General\PaginationRequest;
 use App\Http\Requests\General\SearchPaginationRequest;
+use App\Http\Requests\Namespaces\CreateNamespaceRequest;
+use App\Http\Requests\Namespaces\CreateServiceRequest;
+use App\Http\Requests\Namespaces\UpdateNamespaceRequest;
+use App\Models\Namespaces;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
@@ -11,5 +15,14 @@ interface NamespacesService
 {
     public function getAll(PaginationRequest $data): LengthAwarePaginator | Collection;
 
+    public function getById(int $id): Collection | null;
+
     public function search(SearchPaginationRequest $data): LengthAwarePaginator | Collection;
+
+    public function create(CreateNamespaceRequest $data): Collection;
+
+    public function createService(int $id, CreateServiceRequest $data) : Collection;
+
+    public function update(int $id, UpdateNamespaceRequest $data): Collection;
+    public function delete(int $id): Collection;
 }
