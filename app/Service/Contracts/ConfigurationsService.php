504855e67af43102480822f4de44075633511536
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-11 08:44:17 +0700

    chore: Add ConfigurationsService.php files

---
+++ app/Service/Contracts/ConfigurationsService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service\Contracts;
+
+use App\Http\Requests\Configurations\CreateConfigurationRequest;
+use App\Http\Requests\Configurations\UpdateConfigurationRequest;
+use App\Http\Requests\General\PaginationRequest;
+use App\Http\Requests\General\SearchPaginationRequest;
+use App\Http\Requests\ServiceEnvironment\CreateServiceEnvironmentRequest;
+use App\Http\Requests\ServiceEnvironment\UpdateServiceEnvironmentRequest;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+
+interface ConfigurationsService
+{
+    public function getAll(PaginationRequest $data): LengthAwarePaginator | Collection;
+
+    public function getById(int $id): Collection;
+
+    public function search(SearchPaginationRequest $data): LengthAwarePaginator | Collection;
+
+    public function create(CreateConfigurationRequest $data): Collection;
+    public function update(int $id, UpdateConfigurationRequest $data): Collection;
+    public function delete(int $id): Collection;
+}
