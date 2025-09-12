c0e435fdd6fcb206da319dfcad905896a320e4ff
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-12 15:00:32 +0700

    chore: Update Controller.php files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\AppServiceException;
+use App\Exceptions\PermissionDeniedServiceException;
+use App\Exceptions\UnauthorizedServiceException;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 abstract class Controller
 {
-    //
+    public function handleUnauthorizedAndPermissionDenied(
+        AppServiceException $exception,
+        Request $request
+    ): ?RedirectResponse {
+        if ($exception instanceof UnauthorizedServiceException
+            || $exception instanceof PermissionDeniedServiceException) {
+
+            Auth::guard('web')->logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            return redirect()->route('login')->with('error', $exception->getMessage());
+        }
+
+        return null;
+    }
 }
