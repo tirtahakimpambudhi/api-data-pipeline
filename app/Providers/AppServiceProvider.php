61198cc781b6256b2a59db1cdca0940a27d0ed31
thoriqwildan <wildanthoriq14@gmail.com>
2025-09-12 08:28:42 +0700

    fix: is not instantiable with building

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Service\Contracts\ChannelsService;
 use App\Service\Contracts\EnvironmentsService;
 use App\Service\Contracts\NamespacesService;
+use App\Service\Contracts\ServicesService;
+use App\Service\Implements\ChannelsServiceImpl;
 use App\Service\Implements\EnvironmentsServiceImpl;
 use App\Service\Implements\NamespacesServiceImpl;
+use App\Service\Implements\ServicesServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,16 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             EnvironmentsService::class,
             EnvironmentsServiceImpl::class
+        );
+
+        $this->app->bind(
+            ChannelsService::class,
+            ChannelsServiceImpl::class
+        );
+
+        $this->app->bind(
+            ServicesService::class,
+            ServicesServiceImpl::class
         );
     }
 
