19c829300f86441d5225ca6e88a796ddfe6e9947
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-12 15:00:46 +0700

    chore: Update ServicesEnvironmentsService.php files

--- app/Service/Contracts/ServicesEnvironmentsService.php
+++ app/Service/Contracts/ServicesEnvironmentsService.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Collection;
 
 interface ServicesEnvironmentsService
 {
-    public function getAll(PaginationRequest $data): LengthAwarePaginator | Collection;
+    public function getAll(PaginationRequest | null $data, bool $onlySvcEnv = false): LengthAwarePaginator | Collection;
 
     public function getById(int $id): Collection;
 
-    public function search(SearchPaginationRequest $data): LengthAwarePaginator | Collection;
+    public function search(SearchPaginationRequest | null $data, bool $onlySvcEnv = false): LengthAwarePaginator | Collection;
 
     public function create(CreateServiceEnvironmentRequest $data): Collection;
     public function update(int $id, UpdateServiceEnvironmentRequest $data): Collection;
