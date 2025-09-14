f631b6cf69b644625020c19a73105cf19ccab746
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-14 17:18:24 +0700

    chore: Update Controller.php files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,15 +8,29 @@ use App\Exceptions\UnauthorizedServiceException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Inertia\Response;
 
 abstract class Controller
 {
+    public function emptyPaginated(): array
+    {
+        return [
+            'data' => [],
+            'meta' => [
+                'total'        => 0,
+                'per_page'     => 0,
+                'current_page' => 1,
+                'last_page'    => 1,
+            ],
+        ];
+    }
+
+
     public function handleUnauthorizedAndPermissionDenied(
         AppServiceException $exception,
         Request $request
     ): ?RedirectResponse {
-        if ($exception instanceof UnauthorizedServiceException
-            || $exception instanceof PermissionDeniedServiceException) {
+        if ($exception instanceof UnauthorizedServiceException ) {
 
             Auth::guard('web')->logout();
             $request->session()->invalidate();
@@ -25,6 +39,15 @@ abstract class Controller
             return redirect()->route('login')->with('error', $exception->getMessage());
         }
 
+        if ($exception instanceof PermissionDeniedServiceException ) {
+            return redirect()->route('dashboard')->with('error', $exception->getMessage());
+        }
+
         return null;
+    }
+
+    public function inertiaWithStatus(Response $resp, int $status)
+    {
+        return $resp->toResponse(request())->setStatusCode($status);
     }
 }
