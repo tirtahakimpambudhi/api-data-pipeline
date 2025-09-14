1239c1985ffa01e0ead93f6243f4481b27240849
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-14 17:18:21 +0700

    chore: Update PermissionDeniedServiceException.php files

--- app/Exceptions/PermissionDeniedServiceException.php
+++ app/Exceptions/PermissionDeniedServiceException.php
@@ -4,5 +4,9 @@ namespace App\Exceptions;
 
 class PermissionDeniedServiceException extends AppServiceException
 {
-
+    public function __construct(
+        string $message
+    ) {
+        parent::__construct($message, 403);
+    }
 }
