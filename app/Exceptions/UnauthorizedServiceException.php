418e819cf6f8ea63a574d4e23bae35f8fdd7517d
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-14 17:18:22 +0700

    chore: Update UnauthorizedServiceException.php files

--- app/Exceptions/UnauthorizedServiceException.php
+++ app/Exceptions/UnauthorizedServiceException.php
@@ -4,5 +4,9 @@ namespace App\Exceptions;
 
 class UnauthorizedServiceException extends AppServiceException
 {
-
+    public function __construct(
+        string $message
+    ) {
+        parent::__construct($message, 401);
+    }
 }
