bb686205c94647665ccef1d3d7dbb45189f26ad6
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-15 16:11:06 +0700

    chore: Update utils.ts files

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function numberItemOnPage(currentPage : number, sizePage :number) {
+    const offset = (currentPage- 1) * sizePage;
+    return function(index : number) :number {
+        return offset + index + 1
+    }
+}
