0ce41b8b60fb0b55174f8407899481b4d041cac7
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-17 14:22:46 +0700

    chore: Update verify-email.tsx files

--- resources/js/pages/auth/verify-email.tsx
+++ resources/js/pages/auth/verify-email.tsx
@@ -1,18 +1,30 @@
 // Components
 import EmailVerificationNotificationController from '@/actions/App/Http/Controllers/Auth/EmailVerificationNotificationController';
 import { logout } from '@/routes';
-import { Form, Head } from '@inertiajs/react';
+import { Form, Head, usePage } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 
 import TextLink from '@/components/text-link';
 import { Button } from '@/components/ui/button';
 import AuthLayout from '@/layouts/auth-layout';
+import { useFlash } from '@/hooks/use-flash';
 
-export default function VerifyEmail({ status }: { status?: string }) {
+type Props = {
+    status?: string
+    flash?: {
+        error?: string;
+        success?: string;
+        message?: string;
+    };
+}
+
+export default function VerifyEmail({ status }: Props) {
+    const {props} = usePage<Props>();
+    useFlash(props?.flash);
     return (
         <AuthLayout title="Verify email" description="Please verify your email address by clicking on the link we just emailed to you.">
             <Head title="Email verification" />
-
+            <Toaster richColors position="top-right" />
             {status === 'verification-link-sent' && (
                 <div className="mb-4 text-center text-sm font-medium text-green-600">
                     A new verification link has been sent to the email address you provided during registration.
