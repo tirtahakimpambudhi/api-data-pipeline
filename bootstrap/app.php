fc45325cb958ea5b5b5a13fc670c4b89d963f1fb
tirta@ainosi.com <tirta@ainosi.com>
2025-10-24 08:14:56 +0700

    feature(configuration): register cron job that executes configuration command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Console\Commands\RunConfiguration;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Models\Configurations;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Support\Facades\Schema;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withCommands([
+        RunConfiguration::class,
+    ])
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
@@ -21,6 +28,24 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        if (!Schema::hasTable('configurations')) {
+            return;
+        }
+        $configs = Configurations::all();
+        foreach ($configs as $config) {
+            if (!$config->cron_expression) continue;
+            try {
+                new \Cron\CronExpression($config->cron_expression);
+            } catch (Throwable $e) {
+                continue;
+            }
+            $schedule->command("configuration:run {$config->id} --method=sequential -v")
+                ->cron($config->cron_expression)
+                ->withoutOverlapping()
+                ->onOneServer();
+        }
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
