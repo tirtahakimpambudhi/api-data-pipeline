14f085ebd3609767c2b331c5627feb347fb0bda0
tirta@ainosi.com <tirta@ainosi.com>
2025-10-24 08:14:56 +0700

    feature(configuration): register transform service and inject transform dependencies into configuration service

--- app/Providers/ServiceLayerProvider.php
+++ app/Providers/ServiceLayerProvider.php
@@ -2,6 +2,19 @@
 
 namespace App\Providers;
 
+
+use App\Service\Contracts\TransformService;
+use App\Service\Implements\TransformServiceImpl;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+
+use App\Models\Channels;
+use App\Models\Configurations;
+use App\Models\Environments;
+use App\Models\Namespaces;
+use App\Models\Services;
+use App\Models\ServicesEnvironments;
 use App\Service\Contracts\ChannelsService;
 use App\Service\Contracts\ConfigurationsService;
 use App\Service\Contracts\EnvironmentsService;
@@ -15,22 +28,76 @@ use App\Service\Implements\NamespacesServiceImpl;
 use App\Service\Implements\ServicesEnvironmentsServiceImpl;
 use App\Service\Implements\ServicesServiceImpl;
 use Illuminate\Contracts\Auth\Factory as AuthFactory;
-use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Support\ServiceProvider;
-
 use Illuminate\Log\Logger;
-use App\Models\Namespaces;
-use App\Models\Services;
-use App\Models\Environments;
-use App\Models\ServicesEnvironments;
-use App\Models\Channels;
-use App\Models\Configurations;
+use Mustache\Engine;
 
 class ServiceLayerProvider extends ServiceProvider implements DeferrableProvider
 {
     public function register(): void
     {
+        // Namespaces
+        $this->app->bind(NamespacesService::class, function ($app) {
+            return new NamespacesServiceImpl(
+                $app->make(AuthFactory::class),
+                $app->make(Namespaces::class),
+                $app->make(Logger::class),
+            );
+        });
 
+        // Services
+        $this->app->bind(ServicesService::class, function ($app) {
+            return new ServicesServiceImpl(
+                $app->make(AuthFactory::class),
+                $app->make(Services::class),
+                $app->make(Logger::class),
+            );
+        });
+
+        // Environments
+        $this->app->bind(EnvironmentsService::class, function ($app) {
+            return new EnvironmentsServiceImpl(
+                $app->make(AuthFactory::class),
+                $app->make(Environments::class),
+                $app->make(Logger::class),
+            );
+        });
+
+        // ServicesEnvironments
+        $this->app->bind(ServicesEnvironmentsService::class, function ($app) {
+            return new ServicesEnvironmentsServiceImpl(
+                $app->make(AuthFactory::class),
+                $app->make(ServicesEnvironments::class),
+                $app->make(Logger::class),
+            );
+        });
+
+        // Channels
+        $this->app->bind(ChannelsService::class, function ($app) {
+            return new ChannelsServiceImpl(
+                $app->make(AuthFactory::class),
+                $app->make(Channels::class),
+                $app->make(Logger::class),
+            );
+        });
+
+
+        // Transforms
+        $this->app->bind(TransformService::class, function ($app) {
+            return new TransformServiceImpl(
+              $app->make(Engine::class),
+              $app->make(Logger::class),
+            );
+        });
+
+        // Configurations
+        $this->app->bind(ConfigurationsService::class, function ($app) {
+            return new ConfigurationsServiceImpl(
+                $app->make(AuthFactory::class),
+                $app->make(Configurations::class),
+                $app->make(Logger::class),
+                $app->make(TransformService::class),
+            );
+        });
     }
 
     public function boot(): void
@@ -41,7 +108,13 @@ class ServiceLayerProvider extends ServiceProvider implements DeferrableProvider
     public function provides(): array
     {
         return [
-
+            NamespacesService::class,
+            ServicesService::class,
+            EnvironmentsService::class,
+            ServicesEnvironmentsService::class,
+            ChannelsService::class,
+            ConfigurationsService::class,
+            TransformService::class
         ];
     }
 }
