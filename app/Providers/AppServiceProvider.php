9e0a3f2b5fc7fd9e007fc146704d1ebe194b0dd5
tirta@ainosi.com <tirta@ainosi.com>
2025-10-24 08:14:55 +0700

    feature(configuration): provide Mustache library via service provider for transforming mustache syntax

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Channels;
-use App\Models\Configurations;
-use App\Models\Environments;
-use App\Models\Namespaces;
-use App\Models\Services;
-use App\Models\ServicesEnvironments;
-use App\Service\Contracts\ChannelsService;
-use App\Service\Contracts\ConfigurationsService;
-use App\Service\Contracts\EnvironmentsService;
-use App\Service\Contracts\NamespacesService;
-use App\Service\Contracts\ServicesEnvironmentsService;
-use App\Service\Contracts\ServicesService;
-use App\Service\Implements\ChannelsServiceImpl;
-use App\Service\Implements\ConfigurationsServiceImpl;
-use App\Service\Implements\EnvironmentsServiceImpl;
-use App\Service\Implements\NamespacesServiceImpl;
-use App\Service\Implements\ServicesEnvironmentsServiceImpl;
-use App\Service\Implements\ServicesServiceImpl;
-use Illuminate\Contracts\Auth\Factory as AuthFactory;
-use Illuminate\Log\Logger;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,59 +11,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Namespaces
-        $this->app->bind(NamespacesService::class, function ($app) {
-            return new NamespacesServiceImpl(
-                $app->make(AuthFactory::class),
-                $app->make(Namespaces::class),
-                $app->make(Logger::class),
-            );
+        $this->app->singleton(\Mustache\Engine::class, function () {
+            return new \Mustache\Engine([
+                'escape' => function ($value) {
+                    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+                },
+                'entity_flags' => ENT_QUOTES
+            ]);
         });
 
-        // Services
-        $this->app->bind(ServicesService::class, function ($app) {
-            return new ServicesServiceImpl(
-                $app->make(AuthFactory::class),
-                $app->make(Services::class),
-                $app->make(Logger::class),
-            );
-        });
-
-        // Environments
-        $this->app->bind(EnvironmentsService::class, function ($app) {
-            return new EnvironmentsServiceImpl(
-                $app->make(AuthFactory::class),
-                $app->make(Environments::class),
-                $app->make(Logger::class),
-            );
-        });
-
-        // ServicesEnvironments
-        $this->app->bind(ServicesEnvironmentsService::class, function ($app) {
-            return new ServicesEnvironmentsServiceImpl(
-                $app->make(AuthFactory::class),
-                $app->make(ServicesEnvironments::class),
-                $app->make(Logger::class),
-            );
-        });
-
-        // Channels
-        $this->app->bind(ChannelsService::class, function ($app) {
-            return new ChannelsServiceImpl(
-                $app->make(AuthFactory::class),
-                $app->make(Channels::class),
-                $app->make(Logger::class),
-            );
-        });
-
-        // Configurations
-        $this->app->bind(ConfigurationsService::class, function ($app) {
-            return new ConfigurationsServiceImpl(
-                $app->make(AuthFactory::class),
-                $app->make(Configurations::class),
-                $app->make(Logger::class),
-            );
-        });
     }
 
     /**
